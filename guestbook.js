244d54596f1d9968144ad98c598395eda1418e2e
Anam020990 <anamikaa@skillup.tech>
2021-06-30 07:46:01 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://1e360a15.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
